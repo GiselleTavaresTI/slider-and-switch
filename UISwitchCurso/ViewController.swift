c719966f4319486c4fe4fa7366a0216269f16aa2
Giselle dos Santos Tavares <gisellesantos13@hotmail.com>
2025-05-20 00:28:32 -0300

    Inicializando novo repositório Git

---
+++ UISwitchCurso/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  UISwitchCurso
+//
+//  Created by Giselle Dos Santos Tavares on 19/05/25.
+
+//  Interação Simples: O usuário interage com o Switch geralmente tocando ou clicando nele. Essa ação faz com que o controle alterne instantaneamente entre os estados ligado e desligado. A animação do deslizamento da chave fornece um feedback visual imediato da mudança de estado.
+// O Slider no Xcode (UISlider no iOS, NSSlider no macOS) é um controle visual que permite ao usuário selecionar um valor contínuo dentro de um intervalo definido, arrastando um indicador ao longo de uma barra. É usado para ajustar configurações como volume, brilho ou aplicar filtros com intensidades variáveis.
+
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var stateSwitch: UISwitch!
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        stateSwitch.isOn = false
+        
+        slider.minimumValue = 0
+        slider.value = 0
+        slider.maximumValue = 20
+        
+        
+        
+        
+    }
+
+
+    @IBAction func tappedSwitchAction(_ sender: UISwitch) {
+        }
+    
+    @IBAction func tappedSlider(_ sender: UISlider) {
+        print(Int(sender.value))
+    }
+    
+}
+
+
---
+++ UISwitchCursoTests/UISwitchCursoTests.swift
@@ -0,0 +1,17 @@
+//
+//  UISwitchCursoTests.swift
+//  UISwitchCursoTests
+//
+//  Created by Giselle Dos Santos Tavares on 19/05/25.
+//
+
+import Testing
+@testable import UISwitchCurso
+
+struct UISwitchCursoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
